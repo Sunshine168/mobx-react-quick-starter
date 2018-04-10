73a0be960c7290530b61a7f2edf4e3daac9bf455
mai <168496714@qq.com>
2018-04-10 11:28:30 +0800

    Added code split

--- src/router.js
+++ src/router.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
 import DevTools from 'mobx-react-devtools'
+import Loadable from 'react-loadable'
+import Loading from './component/loading'
 
-import Counter from './screen/demo'
+const LoadableComponent = Loadable({
+  loader: () => import('./screen/demo'),
+  loading: Loading,
+})
 
 class AppRouter extends React.Component {
   render() {
     return (
       <div>
-        <Route path='/counter' component={Counter} />
+        <Route path='/counter' component={LoadableComponent} />
         <DevTools />
       </div>
     )
