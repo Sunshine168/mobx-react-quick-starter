a1482bef9d36555d5d5237f225db03839d2f003e
mai <168496714@qq.com>
2018-04-10 10:38:48 +0800

    Update prod config

--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -7,6 +7,7 @@ const MinifyPlugin = require('babel-minify-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 const ParallelUglifyPlugin = require('webpack-parallel-uglify-plugin')
 const HappyPack = require('happypack')
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 const os = require('os')
 
 const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
@@ -14,7 +15,7 @@ const happyThreadPool = HappyPack.ThreadPool({ size: os.cpus().length })
 module.exports = merge(baseConfig, {
   entry: {
     main: path.resolve(__dirname, '../main.js'),
-    vendor: ['react'],
+    vendor: ['react', 'react-router', 'mobx', 'mobx-react'],
   },
   output: {
     path: path.join(__dirname, '../build'),
@@ -23,6 +24,7 @@ module.exports = merge(baseConfig, {
     publicPath: '/',
   },
   optimization: {
+    runtimeChunk: true,
     splitChunks: {
       name: 'vendor',
     },
@@ -58,21 +60,38 @@ module.exports = merge(baseConfig, {
       },
     }),
     new webpack.HashedModuleIdsPlugin(),
-    new ParallelUglifyPlugin({
-      cacheDir: '.cache/',
-      uglifyJS: {
-        output: {
-          comments: false,
-        },
-        compress: {
-          warnings: false,
-        },
-      },
-    }),
     new HappyPack({
       id: 'js-happypack',
       threadPool: happyThreadPool,
       loaders: ['babel-loader?cacheDirectory=true'],
+    }),
+    new UglifyJsPlugin({
+      uglifyOptions: {
+        ecma: 8,
+        compress: {
+          warnings: false,
+          // Disabled because of an issue with Uglify breaking seemingly valid code:
+          // https://github.com/facebook/create-react-app/issues/2376
+          // Pending further investigation:
+          // https://github.com/mishoo/UglifyJS2/issues/2011
+          comparisons: false,
+        },
+        mangle: {
+          safari10: true,
+        },
+        output: {
+          comments: false,
+          // Turned on because emoji and regex is not minified properly using default
+          // https://github.com/facebook/create-react-app/issues/2488
+          ascii_only: true,
+        },
+      },
+      // Use multi-process parallel running to improve the build speed
+      // Default number of concurrent runs: os.cpus().length - 1
+      parallel: true,
+      // Enable file caching
+      cache: true,
+      sourceMap: true,
     }),
   ],
 })
