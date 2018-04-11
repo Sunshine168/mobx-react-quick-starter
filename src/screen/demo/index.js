854c7d18f240e6c45e81de848e9c7382875aa12b
mai <168496714@qq.com>
2018-04-11 17:01:01 +0800

    Fixed rep

--- src/screen/demo/index.js
+++ src/screen/demo/index.js
@@ -2,7 +2,6 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 import { inject, observer } from 'mobx-react'
-import { decorate, observable, action } from 'mobx'
 
 import { Container } from './component'
 
@@ -25,25 +24,16 @@ const TestElement = styled.div`
 }))
 @observer
 class Demo extends Component<Props> {
-  tempCount = 0
-
-  @action.bound
-  tempIncrease() {
-    this.tempCount += 1
-  }
-
   render() {
     return (
       <Container>
         <button onClick={this.props.increase}>+</button>
         <button onClick={this.props.decrease}>-</button>
         <div>{this.props.count}</div>
-        <div>{this.tempCount}</div>
         <TestElement />
       </Container>
     )
   }
 }
 
-// react-hot-loader work with  mobx decorator in new version seem break
-export default decorate(Demo, { tempCount: observable })
+export default Demo
